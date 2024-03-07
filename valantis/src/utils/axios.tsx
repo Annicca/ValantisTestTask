658b6783d4dc15295b37e58a193838c126773b91
annica <as@hawkingbros.com>
2024-03-07 21:02:01 +0300

    change base url

--- valantis/src/utils/axios.tsx
+++ valantis/src/utils/axios.tsx
@@ -5,7 +5,7 @@ const password = 'Valantis'
 const date: string = new Date().toISOString().split('T')[0].replace(/-/g,"")
 
 export const instance = axios.create({
-    baseURL: 'http://api.valantis.store:40000/',
+    baseURL: 'https://api.valantis.store:41000/',
     headers: {
         "Content-Type": "application/json",
         'X-Auth': `${md5(`${password}_${date}`)}`
